657f5d335abe5b73aaa6f64f6d356ccb14362051
buku.ch <buku.ch@alibaba-inc.com>
2023-08-18 00:17:17 +0800

    翻转链表

---
+++ src/main/java/org/example/hot100/ReverseLinkedList.java
@@ -0,0 +1,36 @@
+package org.example.hot100;
+
+import java.util.List;
+
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
