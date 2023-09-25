faa3142d09843ec82668296aa194dd920d14264c
buku.ch <buku.ch@alibaba-inc.com>
2023-09-25 08:27:15 +0800

    矩阵置零

---
+++ src/main/java/org/example/hot100/SetZeroes.java
@@ -0,0 +1,65 @@
+package org.example.hot100;
+
+/**
+ * 73. 矩阵置零
+ * https://leetcode.cn/problems/set-matrix-zeroes/description/?envType=study-plan-v2&envId=top-100-liked
+ * @author buku.ch
+ * @Desc
+ * @date 2023/9/25 08:14
+ */
+public class SetZeroes {
+
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        boolean colHasZero = false;
+        boolean rowHasZero = false;
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                colHasZero = true;
+            }
+        }
+        for (int i = 0; i < col; i++) {
+            if (matrix[0][i] == 0) {
+                rowHasZero = true;
+            }
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < col; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < col; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 0; j < row; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
+        if (rowHasZero) {
+            for (int i = 0; i < col; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if (colHasZero) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+
+    }
+
+}
