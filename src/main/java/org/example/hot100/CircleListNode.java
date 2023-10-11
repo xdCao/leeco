a6543eb0130e1ee58e8e6588fe7e90f4988cf5a9
buku.ch <buku.ch@alibaba-inc.com>
2023-10-11 23:56:25 +0800

    环形链表

---
+++ src/main/java/org/example/hot100/CircleListNode.java
@@ -0,0 +1,55 @@
+package org.example.hot100;
+
+/**
+ * @author buku.ch
+ * @Desc
+ * @date 2023/10/11 23:36
+ */
+public class CircleListNode {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        boolean has = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                has = true;
+                break;
+            }
+        }
+        if (!has) {
+            return null;
+        }
+        if (slow == head) {
+            return slow;
+        }
+        fast = head;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+            if (slow == fast) {
+                return slow;
+            }
+        }
+        return null;
+
+    }
+
+}
