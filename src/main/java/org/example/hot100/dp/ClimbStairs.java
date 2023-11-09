83278990efcbf58d8550044cbe8b22d7db3ac67c
buku.ch <buku.ch@alibaba-inc.com>
2023-11-09 23:07:38 +0800

    70. 爬楼梯

---
+++ src/main/java/org/example/hot100/dp/ClimbStairs.java
@@ -0,0 +1,21 @@
+package org.example.hot100.dp;
+
+/**
+ * 70. 爬楼梯
+ * @author buku.ch
+ * @Desc
+ * @date 2023/11/9 22:54
+ */
+public class ClimbStairs {
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i -2];
+        }
+        return dp[n];
+    }
+
+}
